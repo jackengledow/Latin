e6d1777607b5cd3065db03ecaad8063fda65cc51
Jack <jmengledow@indy.rr.com>
2017-05-10 14:58:10 -0400

    tabs instead of pages

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 var checkC = function(){
 	var answer = document.getElementById("c").checked;
-	if(answer == true){
+	if(answer === true){
 		$('img').remove();
 		$("div").append("<p>Correct!</p>")
 	}
-	else if (answer == false) {
+	else if (answer === false) {
 		$('img').remove();
 		$("div").append("<p>Definitely wrong.</p>")
 	}
 }
 var checkD = function(){
 	var answer = document.getElementById("d").checked;
-	if(answer == true){
+	if(answer === true){
 		$('img').remove();
 		$("div").append("<p>Correct!</p>")
 	}
-	else if (answer == false) {
+	else if (answer === false) {
 		$('img').remove();
 		$("div").append("<p>Definitely wrong.</p>")
 	}
 }
 var checkB = function(){
 	var answer = document.getElementById("b").checked;
-	if(answer == true){
+	if(answer === true){
 		$('img').remove();
 		$("div").append("<p>Correct!</p>")
 	}
-	else if (answer == false) {
+	else if (answer === false) {
 		$('img').remove();
 		$("div").append("<p>Definitely wrong.</p>")
 	}
 }
 var checkA = function(){
 	var answer = document.getElementById("a").checked;
-	if(answer == true){
+	if(answer === true){
 		$('img').remove();
 		$("div").append("<p>Correct!</p>")
 	}
-	else if (answer == false) {
+	else if (answer === false) {
 		$('img').remove();
 		$("div").append("<p>Definitely wrong.</p>")
 	}
